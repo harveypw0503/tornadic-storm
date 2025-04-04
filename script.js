3345f30c8f8b88f39fab3cd25a50561eaaa41876
harveypw0503 <harveypw0503@gmail.com>
2025-04-04 10:41:31 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ function checkPassword() {
     const message = document.getElementById("message");
 
     if (password.toUpperCase() === correctPassword) {
+        // Store a flag in localStorage to indicate the user has authenticated
+        localStorage.setItem("authenticated", "true");
+        // Redirect to home.html
         window.location.href = "home.html";
     } else {
         message.textContent = "Incorrect password. Try again.";
